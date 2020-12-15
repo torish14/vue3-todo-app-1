78e73efcbe9c1ca26d94e12070906196ccb5df66
azukiazusa1 <cycleippei@gmail.com>
2020-12-16 00:16:55 +0900

    todo-item component

--- src/views/todos.vue
+++ src/views/todos.vue
@@ -1,17 +1,25 @@
 <template>
   <h2>TODO一覧</h2>
   <ul>
-    <li v-for="todo in todoStore.state.todos" :key="todo.id">
-      {{ todo.title }}
-    </li>
+    <todo-item
+      v-for="todo in todoStore.state.todos"
+      :key="todo.id"
+      :todo="todo"
+    >
+    </todo-item>
   </ul>
+  <router-link to="/new">新規作成</router-link>
 </template>
 
 <script lang="ts">
 import { defineComponent, inject } from 'vue'
+import TodoItem from '@/components/TodoItem.vue'
 import { todoKey } from '@/store/todo'
 
 export default defineComponent({
+  components: {
+    TodoItem,
+  },
   setup() {
     const todoStore = inject(todoKey)
     if (!todoStore) {

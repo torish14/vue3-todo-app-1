2930c169cb5a3e6e41a63dc257e3e0e82ce7c72a
azukiazusa1 <cycleippei@gmail.com>
2020-12-23 00:19:22 +0900

    asynctodos

---
+++ src/components/AsyncTodos.vue
@@ -0,0 +1,46 @@
+<template>
+  <ul>
+    <todo-item
+      v-for="todo in todoStore.state.todos"
+      :key="todo.id"
+      :todo="todo"
+      @click-title="clickTitle"
+      @click-delete="clickDelete"
+    >
+    </todo-item>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent, inject } from 'vue'
+import { useRouter } from 'vue-router'
+import TodoItem from '@/components/TodoItem.vue'
+import { todoKey } from '@/store/todo'
+
+export default defineComponent({
+  components: {
+    TodoItem,
+  },
+  async setup() {
+    const todoStore = inject(todoKey)
+    if (!todoStore) {
+      throw new Error('todoStore is not provided')
+    }
+    const router = useRouter()
+    const clickDelete = (id: number) => {
+      todoStore.deleteTodo(id)
+    }
+    const clickTitle = (id: number) => {
+      router.push(`/edit/${id}`)
+    }
+
+    await todoStore.fetchTodos()
+
+    return {
+      todoStore,
+      clickDelete,
+      clickTitle,
+    }
+  },
+})
+</script>
